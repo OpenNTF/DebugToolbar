76c10d618e535d0cd09e2e11c64e2f6f48f5534a
Mark Leusink <m.leusink@gmail.com>
2014-02-05 12:03:39 +0100

    plugin code cleanup: Activator removed

--- org.openntf.xsp.debugtoolbar/src/org/openntf/xsp/debugtoolbar/library/DebugToolbarLibrary.java
+++ org.openntf.xsp.debugtoolbar/src/org/openntf/xsp/debugtoolbar/library/DebugToolbarLibrary.java
@@ -1,40 +1,13 @@
-/*
- * © Copyright GBS Inc 2011
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at:
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
- * implied. See the License for the specific language governing 
- * permissions and limitations under the License.
- */
 package org.openntf.xsp.debugtoolbar.library;
-
-import org.openntf.xsp.debugtoolbar.Activator;
 
 import com.ibm.xsp.library.AbstractXspLibrary;
 
 public class DebugToolbarLibrary extends AbstractXspLibrary {
-	
-	private final static String LIBRARY_ID = DebugToolbarLibrary.class.getPackage().getName();
 
-	private final static boolean _debug = Activator._debug;
-
-	static {
-		if (_debug) {
-			System.out.println(DebugToolbarLibrary.class.getName() + " loaded");
-		}
-	}
+	private final static String LIBRARY_ID = DebugToolbarLibrary.class
+			.getPackage().getName();
 
 	public DebugToolbarLibrary() {
-		if (_debug) {
-			System.out.println(DebugToolbarLibrary.class.getName() + " created");
-		}
 	}
 
 	public String getLibraryId() {
@@ -43,7 +16,7 @@ public class DebugToolbarLibrary extends AbstractXspLibrary {
 
 	@Override
 	public String getPluginId() {
-		return Activator.PLUGIN_ID;
+		return "org.openntf.xsp.debugtoolbar";
 	}
 
 	@Override
@@ -55,21 +28,15 @@ public class DebugToolbarLibrary extends AbstractXspLibrary {
 
 	@Override
 	public String[] getXspConfigFiles() {
-		String[] files = new String[] { 
-				"META-INF/debugToolbar.xsp-config"};
+		String[] files = new String[] { "META-INF/debugToolbar.xsp-config" };
 
 		return files;
 	}
 
 	@Override
 	public String[] getFacesConfigFiles() {
-		String[] files = new String[] { 
-				"META-INF/debugToolbar-faces-config.xml"};
+		String[] files = new String[] { "META-INF/debugToolbar-faces-config.xml" };
 		return files;
 	}
 
-	@Override
-	public boolean isGlobalScope() {
-		return false;
-	}
 }
